4d9c0f3f74bfade29040f588ea6d22e7be1ee1b9
samuelSales00 <samuel.ssouza@sptech.school>
2025-08-26 20:53:34 -0300

    adicionando estruturas de pastas

---
+++ src/main/java/com/atlas/api_risco_credito/ApiRiscoCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.atlas.api_risco_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRiscoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRiscoCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlas/api_risco_credito/ApiRiscoCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.api_risco_credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRiscoCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
